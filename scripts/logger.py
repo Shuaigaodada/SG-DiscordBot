d261ef14676a3305b069b09322ef8e4734a75736
Shuaigaodada <95506889+Shuaigaodada@users.noreply.github.com>
2023-12-07 19:08:18 +0000

    base logger

--- scripts/logger.py
+++ scripts/logger.py
@@ -7,8 +7,17 @@ INFO = "[INFO]"
 WARNING = "[WARNING]"
 ERROR = "[ERROR]"
 
-def log(*content: _Tuple[str], level: str = INFO, sep: str= "", log_name: str = "main.log") -> None:
+def log(*content: _Tuple[str], level: str = INFO, sep: str = "", log_name: str = "main.log") -> None:
     content_list = [str(cstr) for cstr in content]
     info: str = level + ": " + sep.join(content_list)
     with open(_path.join(_LOGS_PATH, log_name), "a") as file:
         file.write(info + "\n")
+
+def info(*content: _Tuple[str], sep: str = "", log_name: str = "main.log") -> None:
+    return log(*content, level=INFO, sep=sep, log_name=log_name)
+
+def warning(*content: _Tuple[str], sep: str = "", log_name: str = "main.log") -> None:
+    return log(*content, level=WARNING, sep=sep, log_name=log_name)
+
+def error(*content: _Tuple[str], sep: str = "", log_name: str = "main.log") -> None:
+    return log(*content, level=ERROR, sep=sep, log_name=log_name)
